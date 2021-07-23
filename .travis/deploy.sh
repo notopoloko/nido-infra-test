0b962de9073a08b65e07808111daf5f3c0c99420
notopoloko <amarals16@hotmail.com>
2021-07-23 04:59:01 -0300

    Another attempt

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
 scp -P ${PORT} -pr ${TRAVIS_BUILD_DIR} ${USER}@${SERVER}:~/${PROJECT}
-command=$(sed "" <<< "cd ${PROJECT} && pwd && ls && docker-compose up --build -d")
+# command=$(sed "" <<< "cd ${PROJECT} && pwd && ls && docker-compose up --build -d")
+command=$(sed "" <<< "cd nido-infra-test && pwd && ls && docker-compose up --build -d")
 ssh ${USER}@${SERVER} -p ${PORT} eval ${command}
