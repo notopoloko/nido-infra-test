190cae82ff55f1aaabd9b821c2cd9a66511c9944
notopoloko <amarals16@hotmail.com>
2021-07-23 04:28:25 -0300

    Build attempt

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -e
 scp -P ${PORT} -pr ${TRAVIS_BUILD_DIR} ${USER}@${SERVER}:~/${PROJECT}
-ssh ${USER}@${SERVER} -p ${PORT} -v 'date && exit'
+command=$(sed "" <<< "cd ${PROJECT} && docker-compose up --build -d")
+ssh ${USER}@${SERVER} -p ${PORT} eval ${command}
